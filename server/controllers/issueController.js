ca2553f96cff37a799032e121149ab7437ae3638
Kevin Chu <chu.kevin.h@gmail.com>
2024-06-13 14:42:52 +1000

    Completed issue controller

--- server/controllers/issueController.js
+++ server/controllers/issueController.js
@@ -11,15 +11,55 @@ const getIssue = async (req,res) => {
 }
 
 const createIssue = async (req,res) => {
-    res.status(200).json("Create Issue")
+    const newIssue = req.body
+
+    try {
+        const issue = await Issue.create(newIssue)
+        res.status(200).json(issue)
+        console.log("Issue data created")
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 const updateIssue = async (req,res) => {
-    res.status(200).json("Update Issue")
+    const { id } = req.params
+    const updatedIssue = req.body
+
+    try {
+        const issue = await Issue.updateOne(
+            { "id": id },
+            { $set: updatedIssue}
+        )
+        res.status(200).json(
+            {
+                message: `Issue data at id: ${id} updated`,
+                issue: issue 
+            }
+        )
+        console.log(`Issue data at id: ${id} updated`)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 const deleteIssue = async (req,res) => {
-    res.status(200).json("Delete Issue")
+    const { id } = req.params
+
+    try {
+        const issue = await Issue.deleteOne(
+            { "id": id },
+        )
+        res.status(200).json(
+            {
+                message: `Issue data at id: ${id} deleted`,
+                issue: issue 
+            }
+        )
+        console.log(`Issue data at id: ${id} deleted`)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 module.exports = {
